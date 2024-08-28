aab4270c9bc960a5be10f1226b8f6eb1a708d750
deniskalimulin <qwe.asd345921@yandex.ru>
2024-08-28 12:44:03 +0300

    first commit

---
+++ src/main/java/Builder/Phone.java
@@ -0,0 +1,70 @@
+/*
+Builder
+ */
+
+public class Phone {
+    private final String brand;
+    private final String model;
+    private final String OS;
+    private final String color;
+    private final int number;
+
+
+    public Phone(String brand, String model, String os, String color, int number) {
+        this.brand = brand;
+        this.model = model;
+        OS = os;
+        this.color = color;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", OS='" + OS + '\'' +
+                ", color='" + color + '\'' +
+                ", number=" + number +
+                '}';
+    }
+
+    public static PhoneBuilder phoneBuilder() {
+        return new PhoneBuilder();
+    }
+
+    public static class PhoneBuilder {
+        private String brand;
+        private String model;
+        private String OS;
+        private String color;
+        private int number;
+
+        public PhoneBuilder withBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public PhoneBuilder withModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public PhoneBuilder withOS(String OS) {
+            this.OS = OS;
+            return this;
+        }
+
+        public PhoneBuilder withColor(String color) {
+            this.color = color;
+            return this;
+        }
+        public PhoneBuilder withNumber(int number) {
+            this.number = number;
+            return this;
+        }
+        public Phone build() {
+            return new Phone(brand, model, OS, color, number);
+        }
+    }
+}
